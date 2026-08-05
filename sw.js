52fc7fd02c7f2ea2aa859f5a0cc7d7ae6c69b609
zecior13 <mioduchowski.radomil@gmail.com>
2026-08-05 14:08:25 +0200

    Rozbuduj przewodnik po muzeach

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nyc-2026-v86";
+const CACHE = "nyc-2026-v90";
 
 // Rdzeń jest mały i musi zostać zapisany w całości, aby aplikacja zawsze się uruchomiła.
 const CORE_ASSETS = [
@@ -7,6 +7,9 @@ const CORE_ASSETS = [
   "./styles.css",
   "./data.js",
   "./museum-data.js",
+  "./met-on-view-data.js",
+  "./museum-expanded-data.js",
+  "./museum-education-data.js",
   "./app.js",
   "./manifest.json",
   "./icons/icon-192.png",
@@ -38,10 +41,16 @@ const MEDIA_ASSETS = [
   "./assets/photos/136.jpg",
   "./assets/photos/137.webp",
   "./assets/photos/138.webp",
+  "./assets/photos/139.jpg",
   "./assets/photos/140.jpg",
   "./assets/photos/141.webp",
   "./assets/photos/142.jpg",
-  "./assets/photos/143.jpg"
+  "./assets/photos/143.jpg",
+  ...Array.from({ length: 105 }, (_, index) =>
+    `./assets/photos/${String(index + 144).padStart(3, "0")}.jpg`
+  ),
+  ...[436123,436138,436139,436140,436141,436150,436152,436153,436168,436524,436525,436526,436527,436529,436530,436531,436532,436533,436534,436535,436536,436945,436946,436952,436960,437385,437387,437388,437390,437392,437393,437395,437396,437397,437399,437402,437406,437424,437425,437426,437428,437429,437430,437432,437436,437437,437980,437984,437998,438002,438010,438011,438012,438013,438014,438722,438815,438817,438819,438857,441104,459082,459098,459123,782304]
+    .map(id => `./assets/photos/met-${id}.jpg`)
 ];
 
 const assetUrl = asset => new URL(asset, self.location.href).href;
